3dcdc675ea377e46c90494eb2d95f1e5acf355aa
Ryan Snow <74696050+r-snow@users.noreply.github.com>
2022-09-02 15:05:19 -0700

    wrote pc

---
+++ counting_inversions_pc.py
@@ -0,0 +1,24 @@
+# create a test array
+
+# create count_inversion function
+  # define split_count
+  # test if array length is greater than one
+    # split array into a left and right half
+
+    # call recursive call on both left and right
+
+    #define index for left, right, and split count
+
+    #while through left index and right index
+      # test if left is less than right
+        # set merged at index to left at index
+        #increment left index
+      # else
+        # set merged at index to right at index
+        #increment right index
+        #increment split_count
+      #increment split count
+  # return split count
+
+# call count_inversion function on test array
+#printtest array
